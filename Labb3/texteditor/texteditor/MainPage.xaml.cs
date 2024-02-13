5b79221af23b401e375fe8c2dd8317a1f246e958
Oskar Deubler <deubler.oskar@gmail.com>
2024-02-13 12:23:05 +0100

    ChangeText

--- Labb3/texteditor/texteditor/MainPage.xaml.cs
+++ Labb3/texteditor/texteditor/MainPage.xaml.cs
@@ -5,13 +5,17 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Pickers;
+using Windows.UI.Popups;
 using Windows.UI.Xaml;
+using Windows.UI.ViewManagement;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using System.Threading.Tasks;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -22,9 +26,125 @@ namespace texteditor
     /// </summary>
     public sealed partial class MainPage : Page
     {
-        public MainPage()
+		Windows.Storage.StorageFile savefile;
+		Boolean unsaved_changes;
+		Boolean fileExists;
+		String filename;
+		public MainPage()
         {
+			filename = "fil";
+			unsaved_changes = false;
+			fileExists = false;
             this.InitializeComponent();
         }
-    }
+
+		private void ClearButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (unsaved_changes)
+			{
+
+			}
+            MainTextBox.Text = string.Empty;
+		}
+
+		private void SaveButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (!fileExists)
+			{
+				SaveAs();
+			} else
+			{
+				Save();
+			}
+		}
+
+		
+		
+		private void OpenButton_Click(object sender, RoutedEventArgs e)
+		{
+			openFile();
+		}
+
+		private void SaveAsButton_Click(object sender, RoutedEventArgs e)
+		{
+			SaveAs();
+		}
+
+		private async void Save()
+		{
+			if (savefile != null)
+			{
+				await Windows.Storage.FileIO.WriteTextAsync(savefile, MainTextBox.Text);
+				unsaved_changes = false;
+				changeTitle(savefile.Name);
+			}
+		}
+
+		private async void openFile()
+		{
+			FileOpenPicker fileOpenPicker = new FileOpenPicker
+			{
+				ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+				SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
+			};
+			fileOpenPicker.FileTypeFilter.Add(".txt");
+			savefile = await fileOpenPicker.PickSingleFileAsync();
+			if(savefile != null)
+			{
+				toggleTextChange(false);
+				unsaved_changes = false;
+				fileExists = true;
+				MainTextBox.Text = await Windows.Storage.FileIO.ReadTextAsync(savefile);
+				changeTitle(savefile.Name);
+				toggleTextChange(true);
+			}
+		}
+
+		private void toggleTextChange(bool v)
+		{
+			if (v)
+			{
+				MainTextBox.TextChanged += MainTextBox_TextChanged; //Från chatGPT
+			} else
+			{
+				MainTextBox.TextChanged -= MainTextBox_TextChanged; //Från chatGPT
+			}
+		}
+
+		private async void SaveAs()
+		{
+			FileSavePicker fileSavePicker = new FileSavePicker
+			{
+				SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
+			};
+			fileSavePicker.FileTypeChoices.Add("Plain Text", new List<string>() {".txt"});
+			fileSavePicker.SuggestedFileName = filename;
+
+			savefile = await fileSavePicker.PickSaveFileAsync();
+			if (savefile != null)
+			{
+				unsaved_changes = false;
+				fileExists = true;
+				await Windows.Storage.FileIO.WriteTextAsync(savefile, MainTextBox.Text);
+				changeTitle(savefile.Name);
+			}
+		}
+		private void MainTextBox_TextChanged(object sender, TextChangedEventArgs e)
+		{
+			if (fileExists && !unsaved_changes)
+			{
+				changeTitle("*" + savefile.Name);
+				unsaved_changes = true;
+			}
+		}
+		private void changeTitle(String title)
+		{
+			ApplicationView.GetForCurrentView().Title = title;
+		}
+
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Exit();
+		}
+	}
 }

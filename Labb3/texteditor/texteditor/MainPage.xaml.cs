95069af6b8b3b66bfaf0e1e68160084cfc5e7219
Oskar Deubler <deubler.oskar@gmail.com>
2024-02-19 12:35:08 +0100

    Version1 done

--- Labb3/texteditor/texteditor/MainPage.xaml.cs
+++ Labb3/texteditor/texteditor/MainPage.xaml.cs
@@ -16,8 +16,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using System.Threading.Tasks;
-
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+using System.Reflection.Metadata;
 
 namespace texteditor
 {
@@ -29,32 +28,98 @@ namespace texteditor
 		private Windows.Storage.StorageFile savefile;
 		private Boolean unsaved_changes;
 		private Boolean fileExists;
+		private String defaultDocname = "nytt_dokument";
 		public MainPage()
         {
 			unsaved_changes = false;
 			fileExists = false;
+			changeTitle(defaultDocname);
             this.InitializeComponent();
-        }
+			Windows.UI.Core.Preview.SystemNavigationManagerPreview.GetForCurrentView().CloseRequested += Close_Click;
+		}
+		private async void Close_Click(object sender, Windows.UI.Core.Preview.SystemNavigationCloseRequestedPreviewEventArgs e)
+		{
+			var deferral = e.GetDeferral(); // Get a deferral because we are awaiting async operations
+											//Från Windows.UI.Core.Preview... i MainPage() till denna rad är från chatGPT.
+			if (unsaved_changes)
+			{
+				await Exit_Dialog("Du har osparade ändringar.");
+
+			}
+
+			deferral.Complete(); //Från chatGPT
+		}
 
 		private void ClearButton_Click(object sender, RoutedEventArgs e)
 		{
-			Clear_Dialog($"All text försvinner om du rensar.");
+			Clear_Dialog("All text försvinner om du rensar.");
 		}
 
-		private async void Clear_Dialog(string msg)
+		private void CloseButton_Click(object sender, RoutedEventArgs e)
 		{
-			MessageDialog sure_Dialog = new MessageDialog(msg);
-			sure_Dialog.Commands.Add(new UICommand("Rensa", x =>
+			if (unsaved_changes)
 			{
-				MainTextBox.Text = string.Empty;
+				Exit_Dialog("Du har osparade ändringar.");
+			} else
+			{
+				Application.Current.Exit();
+			}
+		}
+
+		private void NewButton_Click(object sender, RoutedEventArgs e)
+		{
+			if (unsaved_changes)
+			{ 
+			NewDocument_Dialog("Du har osparade ändringar.");
+			}
+		}
+
+		private async Task Exit_Dialog(string v)
+		{
+			MessageDialog exitDialog = new MessageDialog(v);
+			exitDialog.Title = "Spara innan du avslutar?";
+			exitDialog.Commands.Add(new UICommand("Spara och Avsluta", async x =>
+			{
+				await SaveFile();
+				Application.Current.Exit();
 			}));
-			sure_Dialog.Commands.Add(new UICommand("Avbryt", x =>
+			exitDialog.Commands.Add(new UICommand("Avsluta utan att spara", x =>
+			{
+				Application.Current.Exit();
+			}));
+			exitDialog.Commands.Add(new UICommand("Avbryt", x =>
 			{
 				return;
 			}));
-			await sure_Dialog.ShowAsync();
+			await exitDialog.ShowAsync();
 		}
-
+		private async void NewDocument_Dialog(string v)
+		{
+			MessageDialog newDocDialog = new MessageDialog(v);
+			newDocDialog.Title = "Spara innan du fortsätter?";
+			newDocDialog.Commands.Add(new UICommand("Spara", x =>
+			{
+				SaveFile();
+				NewSheet();
+			}));
+			newDocDialog.Commands.Add(new UICommand("Fortsätt utan att spara", x =>
+			{
+				NewSheet();
+			}));
+			newDocDialog.Commands.Add(new UICommand("Avbryt", x =>
+			{
+				return;
+			}));
+			await newDocDialog.ShowAsync();
+		}
+		private void NewSheet()
+		{
+			savefile = null;
+			MainTextBox.Text = string.Empty;
+			changeTitle(defaultDocname);
+			unsaved_changes = false;
+			fileExists = false;
+		}
 		private void SaveButton_Click(object sender, RoutedEventArgs e)
 		{
 			SaveFile();
@@ -69,20 +134,26 @@ namespace texteditor
 					openFile();
 				}
 		}
-		private void SaveFile()
+		
+		private async void Clear_Dialog(string msg)
 		{
-			if (!fileExists)
+			MessageDialog clearDialog = new MessageDialog(msg);
+			clearDialog.Title = "Rensa text?";
+			clearDialog.Commands.Add(new UICommand("Rensa", x =>
 			{
-				SaveAs();
-			}
-			else
+				MainTextBox.Text = string.Empty;
+			}));
+			clearDialog.Commands.Add(new UICommand("Avbryt", x =>
 			{
-				Save();
-			}
+				return;
+			}));
+			await clearDialog.ShowAsync();
 		}
+
 		private async void DiscardChanges_Dialog(string msg)
 		{
 			MessageDialog discard_Dialog = new MessageDialog(msg);
+			discard_Dialog.Title = "Spara innan?";
 			discard_Dialog.Commands.Add(new UICommand("Spara och Öppna", x =>
 			{
 				SaveFile();
@@ -104,7 +175,19 @@ namespace texteditor
 			SaveAs();
 		}
 
-		private async void Save()
+		private async Task SaveFile()
+		{
+			if (!fileExists)
+			{
+				await SaveAs();
+			}
+			else
+			{
+				await Save();
+			}
+		}
+
+		private async Task Save()
 		{
 			if (savefile != null)
 			{
@@ -114,38 +197,7 @@ namespace texteditor
 			}
 		}
 
-		private async void openFile()
-		{
-			FileOpenPicker fileOpenPicker = new FileOpenPicker
-			{
-				ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
-				SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
-			};
-			fileOpenPicker.FileTypeFilter.Add(".txt");
-			savefile = await fileOpenPicker.PickSingleFileAsync();
-			if(savefile != null)
-			{
-				toggleTextChange(false);
-				unsaved_changes = false;
-				fileExists = true;
-				MainTextBox.Text = await Windows.Storage.FileIO.ReadTextAsync(savefile);
-				changeTitle(savefile.Name);
-				toggleTextChange(true);
-			}
-		}
-
-		private void toggleTextChange(bool v)
-		{
-			if (v)
-			{
-				MainTextBox.TextChanged += MainTextBox_TextChanged; //Från chatGPT
-			} else
-			{
-				MainTextBox.TextChanged -= MainTextBox_TextChanged; //Från chatGPT
-			}
-		}
-
-		private async void SaveAs()
+		private async Task SaveAs()
 		{
 			FileSavePicker fileSavePicker = new FileSavePicker
 			{
@@ -163,6 +215,37 @@ namespace texteditor
 				changeTitle(savefile.Name);
 			}
 		}
+		private async void openFile()
+		{
+			FileOpenPicker fileOpenPicker = new FileOpenPicker
+			{
+				ViewMode = Windows.Storage.Pickers.PickerViewMode.Thumbnail,
+				SuggestedStartLocation = Windows.Storage.Pickers.PickerLocationId.DocumentsLibrary
+			};
+			fileOpenPicker.FileTypeFilter.Add(".txt");
+			savefile = await fileOpenPicker.PickSingleFileAsync();
+			if (savefile != null)
+			{
+				toggleTextChange(false);
+				unsaved_changes = false;
+				fileExists = true;
+				MainTextBox.Text = await Windows.Storage.FileIO.ReadTextAsync(savefile);
+				changeTitle(savefile.Name);
+				toggleTextChange(true);
+			}
+		}
+
+		private void toggleTextChange(bool v)
+		{
+			if (v)
+			{
+				MainTextBox.TextChanged += MainTextBox_TextChanged; //Från chatGPT
+			}
+			else
+			{
+				MainTextBox.TextChanged -= MainTextBox_TextChanged; //Från chatGPT
+			}
+		}
 		private void MainTextBox_TextChanged(object sender, TextChangedEventArgs e)
 		{
 			if (fileExists && !unsaved_changes)
@@ -174,11 +257,6 @@ namespace texteditor
 		private void changeTitle(String title)
 		{
 			ApplicationView.GetForCurrentView().Title = title;
-		}
-
-		private void CloseButton_Click(object sender, RoutedEventArgs e)
-		{
-			Application.Current.Exit();
 		}
 	}
 }

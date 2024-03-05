8e70f5ef70e4b4f0f0d00614fa13be1a86f4d09b
Oskar Deubler <deubler.oskar@gmail.com>
2024-03-05 17:22:10 +0100

    First commit

---
+++ Labb4/Business system/Business system/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_system
+{
+	internal class Book : Product
+	{
+		public string Author { get; set; }
+		public string bookGenre { get; set; }
+		public BookFormat BookFormat { get; set; }
+		public BookLanguage Language { get; set; }
+		public Book(int ID, string Title, int Price) : base(ID, Title, Price)
+		{
+		}
+
+		public Book(int ID, string Title, int Price, int Qty) : base(ID, Title, Price, Qty)
+		{
+		}
+	}
+}
---
+++ Labb4/Business system/Business system/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business_system
+{
+	internal class Product
+	{
+		public int ID { get; set; }
+		public string Name { get; set; }
+		public int Price { get; set; }
+		public int Qty {  get; set; }
+
+		protected Product(int ID, string Title, int Price, int Qty)
+		{
+			this.ID = ID;
+			this.Name = Title;
+			this.Price = Price;
+			this.Qty = Qty;
+		}
+		protected Product(int ID, string Title, int Price)
+		{
+			this.ID = ID;
+			this.Name = Title;
+			this.Price = Price;
+			this.Qty = 0;
+		}
+		public void IncreaseQty(int amount)
+		{
+			//handle negative amount here?
+			Qty += amount;
+		}
+		public void SubtractQty(int amount) {
+			//handle negative qty after subtracting here?
+			Qty -= amount;
+		}
+		
+	}
+}
